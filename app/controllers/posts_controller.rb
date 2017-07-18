5b35af15f05f23e18498c8f9a006016466b690d3
kill5038 <blain1@naver.com>
2017-07-18 16:09:45 +0900

    테스트 끝

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  # before_action :authenticate_user!, except: [ :index, :show ]
-  # layout 'admin'
+  before_action :authenticate_user!, except: [ :index, :show ]
+  layout 'admin'
 
   # GET /posts
   # GET /posts.json
@@ -71,6 +71,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.permit(:title, :content, :user_id)
+      params.require(:post).permit(:title, :content, :user_id)
     end
 end
